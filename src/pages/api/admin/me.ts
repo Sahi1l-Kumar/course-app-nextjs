740267e0a7437d7cf8454c1d9be1e79b1f6a248f
Sahi1l-Kumar <2230189@kiit.ac.in>
2023-11-02 18:34:22 +0530

    Updated me route with auth middleware

--- src/pages/api/admin/me.ts
+++ src/pages/api/admin/me.ts
@@ -1,31 +1,16 @@
+import authenticateMiddleware from "@/lib/auth";
 import { Admin } from "@/lib/db";
 import connectToDB from "@/lib/dbConnect";
-import { verify } from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 
 connectToDB();
 
-const adminSecret = process.env.ADMIN_SECRET || "";
-
-interface jwtPayload {
-  username?: string;
-}
-
-export default async function handler(
+export default authenticateMiddleware(async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const cookies = req.cookies;
-  const token = cookies.authCookie;
-
-  if (!token) {
-    return res.status(403).json({ message: "authCookie not found" });
-  }
-
   try {
-    const user = verify(token, adminSecret) as jwtPayload;
-
-    const admin = await Admin.findOne({ username: user.username });
+    const admin = await Admin.findOne({ username: req.user.username });
 
     if (!admin) {
       return res.status(404).json({ message: "Admin does not exist" });
@@ -35,4 +20,4 @@ export default async function handler(
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
-}
+});
