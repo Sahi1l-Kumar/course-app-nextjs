2fcf1e4c4dde3134a67c058f009262e4221d8ad2
Sahi1l-Kumar <2230189@kiit.ac.in>
2023-11-02 18:33:50 +0530

    Added authentication middleware

---
+++ src/lib/auth.ts
@@ -0,0 +1,40 @@
+import { verify, Secret } from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type UserData = {
+  username: string;
+};
+
+declare module "http" {
+  interface IncomingMessage {
+    user: UserData;
+  }
+}
+
+export default function authenticateMiddleware(
+  handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.authCookie as string;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+      const user = verify(
+        token,
+        process.env.ADMIN_SECRET as Secret
+      ) as unknown as UserData;
+
+      if (user) {
+        req.user = user as UserData;
+        return handler(req, res);
+      } else {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  };
+}
