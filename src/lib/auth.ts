c16a611eed0fff8c3a0cfcd9ecaa4b64371f5b90
Sahi1l-Kumar <2230189@kiit.ac.in>
2023-11-02 19:44:23 +0530

    Added seperate user middleware

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,7 +11,7 @@ declare module "http" {
   }
 }
 
-export default function authenticateMiddleware(
+export function authenticateAdminMiddleware(
   handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>
 ) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
@@ -25,6 +25,34 @@ export default function authenticateMiddleware(
       const user = verify(
         token,
         process.env.ADMIN_SECRET as Secret
+      ) as unknown as UserData;
+
+      if (user) {
+        req.user = user as UserData;
+        return handler(req, res);
+      } else {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  };
+}
+
+export function authenticateUserMiddleware(
+  handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.authCookie as string;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+      const user = verify(
+        token,
+        process.env.USER_SECRET as Secret
       ) as unknown as UserData;
 
       if (user) {
