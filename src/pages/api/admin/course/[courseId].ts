ac460c50109bd44d161e9bb4e09ee8e0acba025b
Sahi1l-Kumar <2230189@kiit.ac.in>
2023-11-02 19:05:15 +0530

    Added Course route

---
+++ src/pages/api/admin/course/[courseId].ts
@@ -0,0 +1,33 @@
+import authenticateMiddleware from "@/lib/auth";
+import { Course } from "@/lib/db";
+import connectToDB from "@/lib/dbConnect";
+import { NextApiRequest, NextApiResponse } from "next";
+
+connectToDB();
+
+export default authenticateMiddleware(async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { courseId } = req.query;
+
+  if (req.method === "GET") {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ course });
+  } else if (req.method === "PUT") {
+    const course = await Course.findByIdAndUpdate(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ message: "Course updated successfully" });
+  } else {
+    const course = await Course.findByIdAndDelete(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ message: "Course deleted successfully" });
+  }
+});
